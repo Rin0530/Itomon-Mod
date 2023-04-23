f047b6b1fcd64281f6c972719826a200bcec0ad1
Rin0530 <51035562+Rin0530@users.noreply.github.com>
2023-04-23 23:55:50 +0900

    アップデートチェッカー実装 (#2)

--- src/main/java/net/rin/itomon/ItomonMod.java
+++ src/main/java/net/rin/itomon/ItomonMod.java
@@ -9,14 +9,24 @@ import net.rin.itomon.init.ItomonModItems;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.forgespi.language.IModInfo;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.VersionChecker;
+import net.minecraftforge.fml.VersionChecker.Status;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentContents;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.contents.TranslatableContents;
 
 import java.util.function.Supplier;
 import java.util.function.Function;
@@ -25,7 +35,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.AbstractMap;
 
-@Mod("itomon")
+@Mod(ItomonMod.MODID)
 public class ItomonMod {
 	public static final Logger LOGGER = LogManager.getLogger(ItomonMod.class);
 	public static final String MODID = "itomon";
@@ -38,6 +48,7 @@ public class ItomonMod {
 		ItomonModItems.REGISTRY.register(bus);
 		ItomonModEntities.REGISTRY.register(bus);
 
+		
 	}
 
 	private static final String PROTOCOL_VERSION = "1";
@@ -69,5 +80,26 @@ public class ItomonMod {
 			actions.forEach(e -> e.getKey().run());
 			workQueue.removeAll(actions);
 		}
+	}
+
+	@SubscribeEvent
+	public void join(ClientPlayerNetworkEvent.LoggingIn event) {
+		Status result = checkUpdate();
+		if (result == Status.OUTDATED){
+			LocalPlayer player = event.getPlayer();
+			String message = 
+			"""
+			Update is available!
+			check https://github.com/Rin0530/Itomon-Mod/releases/latest
+			""";
+			ComponentContents contents = new TranslatableContents(message);
+			Component component = MutableComponent.create(contents);
+			player.sendSystemMessage(component);
+		}
+	}
+
+	public static Status checkUpdate() {
+		IModInfo iModInfo = ModLoadingContext.get().getActiveContainer().getModInfo();
+		return VersionChecker.getResult(iModInfo).status();
 	}
 }
